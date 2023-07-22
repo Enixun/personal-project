740a241419cc1c2bd86c7fdfb1d853f5807d65e8
Christian Ashley <christianzashley@gmail.com>
2023-07-21 22:23:56 -0400

    Update webpack.config to TypeScript

---
+++ webpack.config.ts
@@ -0,0 +1,46 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+export default {
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader'
+        }
+      },
+      {
+        test: /\.s?css/,
+        exclude: /node_modules/,
+        use: ['css-loader', 'scss-loader']
+      },
+      {
+        test: /\.(png|jpg)/,
+        type: 'asset'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devServer: {
+    port: 8080,
+    static: path.resolve(__dirname, 'dist'),
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+      title: 'Personal'
+    })
+  ]
+}
